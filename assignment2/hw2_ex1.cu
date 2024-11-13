b926cc4481f4ac43f69f2c05f495c1d5d0932bd5
Diogo Melita <dgmelita9@gmail.com>
2024-11-13 16:12:43 +0100

    feat: extend ex1 to answer 5.

--- assignment2/hw2_ex1.cu
+++ assignment2/hw2_ex1.cu
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <sys/time.h>
 
@@ -15,12 +14,12 @@ __global__ void vecAdd(DataType *in1, DataType *in2, DataType *out, int len) {
 
 double cpuSecond() {
   struct timeval tp;
-  gettimeofday(&tp,NULL);
-  return ((double)tp.tv_sec + (double)tp.tv_usec*1.e-6);
+  gettimeofday(&tp, NULL);
+  return ((double)tp.tv_sec + (double)tp.tv_usec * 1.e-6);
 }
 
 int main(int argc, char **argv) {
-  
+
   int inputLength;
   DataType *hostInput1;
   DataType *hostInput2;
@@ -34,18 +33,18 @@ int main(int argc, char **argv) {
     fprintf(stderr, "Invalid number of arguments. Usage: ./hw2_ex1 <inputLength>");
     return 1;
   }
-  
+
   inputLength = atoi(argv[1]);
   printf("The input length is %d\n", inputLength);
-  
-  hostInput1 = (DataType*)malloc(inputLength * sizeof(DataType));
-  hostInput2 = (DataType*)malloc(inputLength * sizeof(DataType));
-  hostOutput = (DataType*)malloc(inputLength * sizeof(DataType));
-  resultRef = (DataType*)malloc(inputLength * sizeof(DataType));
-  
+
+  hostInput1 = (DataType *)malloc(inputLength * sizeof(DataType));
+  hostInput2 = (DataType *)malloc(inputLength * sizeof(DataType));
+  hostOutput = (DataType *)malloc(inputLength * sizeof(DataType));
+  resultRef = (DataType *)malloc(inputLength * sizeof(DataType));
+
   for (int i = 0; i < inputLength; i++) {
-    hostInput1[i] = rand()/(DataType)RAND_MAX;
-    hostInput2[i] = rand()/(DataType)RAND_MAX;
+    hostInput1[i] = rand() / (DataType)RAND_MAX;
+    hostInput2[i] = rand() / (DataType)RAND_MAX;
     resultRef[i] = hostInput1[i] + hostInput2[i];
   }
 
@@ -53,20 +52,30 @@ int main(int argc, char **argv) {
   cudaMalloc(&deviceInput2, inputLength * sizeof(DataType));
   cudaMalloc(&deviceOutput, inputLength * sizeof(DataType));
 
+  // Start timing data copy from host to device
+  double startCopyToDevice = cpuSecond();
   cudaMemcpy(deviceInput1, hostInput1, inputLength * sizeof(DataType), cudaMemcpyHostToDevice);
   cudaMemcpy(deviceInput2, hostInput2, inputLength * sizeof(DataType), cudaMemcpyHostToDevice);
+  double elapsedCopyToDevice = cpuSecond() - startCopyToDevice;
 
   dim3 blockSize(TPB, 1, 1);
-  dim3 gridSize((inputLength+TPB-1)/TPB, 1, 1);
+  dim3 gridSize((inputLength + TPB - 1) / TPB, 1, 1);
 
-  double iStart = cpuSecond();
+  // Start timing the CUDA kernel
+  double startKernel = cpuSecond();
   vecAdd<<<gridSize, blockSize>>>(deviceInput1, deviceInput2, deviceOutput, inputLength);
   cudaDeviceSynchronize();
-  double iElaps = cpuSecond() - iStart;
+  double elapsedKernel = cpuSecond() - startKernel;
 
-  printf("Execution time: %f seconds\n", iElaps);
-
+  // Start timing data copy from device to host
+  double startCopyToHost = cpuSecond();
   cudaMemcpy(hostOutput, deviceOutput, inputLength * sizeof(DataType), cudaMemcpyDeviceToHost);
+  double elapsedCopyToHost = cpuSecond() - startCopyToHost;
+
+  // Print individual timings
+  printf("Time for data copy from host to device: %f seconds\n", elapsedCopyToDevice);
+  printf("Time for CUDA kernel execution: %f seconds\n", elapsedKernel);
+  printf("Time for data copy from device to host: %f seconds\n", elapsedCopyToHost);
 
   bool match = true;
   for (int i = 0; i < inputLength; i++) {
